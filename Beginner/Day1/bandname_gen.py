d2eb3de91abcc94d8426e4b4a5b870f026acd686
Esther <esther.wanjikum99@gmail.com>
2023-11-21 09:23:40 +0300

    Day one complete

---
+++ Beginner/Day1/bandname_gen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""generates a band name using a city and a pet name"""
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be {} {}".format(street, pet))
---
+++ Beginner/Day1/variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""switches the values of the variables around"""
+
+a = input("a:")
+b = input("b:")
+temp = a
+a = b
+b = temp
+print("a = " + a)
+print("b = " + b)
